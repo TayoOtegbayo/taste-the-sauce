118d827bd37af4aaaa2dd9d602ccad7b29b6daf1
TayoOtegbayo <tayootegbayo@gmail.com>
2023-08-14 22:23:34 +0100

    Lesson21 confirm exact item id

--- cypress/e2e/product.cy.js
+++ cypress/e2e/product.cy.js
@@ -29,42 +29,44 @@ describe('Product', () => {
     const name = 'Sauce Labs Fleece Jacket'
     const price = '$49.99'
     cy.contains('.inventory_item', name)
-    // confirm the element has "data-itemid" attribute
-    // and its value is a string
-    // https://on.cypress.io/should
-    // https://glebbahmutov.com/cypress-examples/commands/assertions.html
-    //
-    // pass the value of the "data-itemid" attribute into a callback
-    // https://on.cypress.io/then
-    //
-    // find the inventory item again
-    // and inside find the anchor link element
-    // https://on.cypress.io/contains
-    // https://on.cypress.io/find
-    cy.contains('.inventory_item', name)
-      .find('.inventory_item_label a')
-      // Since you know the item's id
-      // you can confirm the exact value of the "id" attribute
-      // (it has item id with title link text)
-      //
-      // click on the anchor link
-      .click()
-    // confirm we transition to the item's page
-    // https://on.cypress.io/location
-    cy.location('pathname').should('equal', '/inventory-item.html')
-    // because we know the item's id
-    // confirm the URL search parameter string
-    // includes "id=item id" substring
-    //
-    // confirm the item details component is visible
-    cy.get('#inventory_item_container .inventory_details')
-      .should('be.visible')
-      .within(() => {
-        // and inside has the item's name (with large size class)
-        // and the item's expected price
-        // https://on.cypress.io/within
-        cy.contains('.inventory_details_name.large_size', name)
-        cy.contains('.inventory_details_price', price)
+      // confirm the element has "data-itemid" attribute
+      // and its value is a string
+      // https://on.cypress.io/should
+      // https://glebbahmutov.com/cypress-examples/commands/assertions.html
+      .should('have.attr', 'data-itemid')
+      .should('be.a', 'string')
+      // pass the value of the "data-itemid" attribute into a callback
+      // https://on.cypress.io/then
+      .then((itemId) => {
+        // find the inventory item again
+        // and inside find the anchor link element
+        // https://on.cypress.io/contains
+        // https://on.cypress.io/find
+        cy.contains('.inventory_item', name)
+          .find('.inventory_item_label a')
+          // Since you know the item's id
+          // you can confirm the exact value of the "id" attribute
+          // (it has item id with title link text)
+          .should('have.attr', 'id', `item_${itemId}_title_link`)
+          // click on the anchor link
+          .click()
+        // confirm we transition to the item's page
+        // https://on.cypress.io/location
+        cy.location('pathname').should('equal', '/inventory-item.html')
+        // because we know the item's id
+        // confirm the URL search parameter string
+        // includes "id=item id" substring
+        //
+        // confirm the item details component is visible
+        cy.get('#inventory_item_container .inventory_details')
+          .should('be.visible')
+          .within(() => {
+            // and inside has the item's name (with large size class)
+            // and the item's expected price
+            // https://on.cypress.io/within
+            cy.contains('.inventory_details_name.large_size', name)
+            cy.contains('.inventory_details_price', price)
+          })
       })
     // go back to the inventory page by clicking
     // "Back to products" button
