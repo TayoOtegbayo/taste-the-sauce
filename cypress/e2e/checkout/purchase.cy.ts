e1e1153ee842270ee5ea0a21e5eb9d45cf64be45
TayoOtegbayo <tayootegbayo@gmail.com>
2025-07-24 21:40:13 +0100

    configuration for different user

--- cypress/e2e/checkout/purchase.cy.ts
+++ cypress/e2e/checkout/purchase.cy.ts
@@ -13,3 +13,20 @@ const item = Cypress._.sample(InventoryData)
 // - add 1 item to the cart
 // - confirm the total price
 // - check out
+describe('All users', () => {
+  Cypress._.each(users, (user: LoginInfo, name) => {
+    it(`works for user persona ${name}`, () => {
+      LoginPage.login(user.username, user.password)
+      cy.visit('/inventory.html')
+      InventoryPage.addItemToCart(item!.name)
+      cy.visit('/checkout-step-one.html')
+      CheckoutPage.fillInformationForm().submit()
+      cy.location('pathname').should('equal', '/checkout-step-two.html')
+      cy.get('.cart_list .cart_item').should('have.length', 1)
+      cy.contains('.summary_subtotal_label', '$' + item?.price)
+      cy.contains('[data-test=finish]', 'Finish').click()
+      cy.location('pathname').should('equal', '/checkout-complete.html')
+      cy.get('#checkout_complete_container').should('be.visible')
+    })
+  })
+})
